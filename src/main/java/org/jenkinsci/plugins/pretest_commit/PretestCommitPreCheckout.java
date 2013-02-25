19c8406df53d9640e60d53006feadac1e9ed31ae
Esben Skaarup <sben@sben.dk>
2013-02-25 22:06:35 +0100

    Added dummy items in the settings to demonstrate different UI elements.
    Added class PretestCommitPreCheckout that demonstrates two different hooks that occur just before the build.
    
    --HG--
    extra : convert_revision : f2a115e7492ad78b9336d5854101fc1f77695dfc

---
+++ src/main/java/org/jenkinsci/plugins/pretest_commit/PretestCommitPreCheckout.java
@@ -0,0 +1,73 @@
+package org.jenkinsci.plugins.pretest_commit;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.Environment;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
+import hudson.model.Cause.LegacyCodeCause;
+import hudson.tasks.BuildWrapper;
+import hudson.tasks.BuildTrigger;
+import hudson.tasks.BuildStep;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class PretestCommitPreCheckout extends BuildWrapper {
+
+	@DataBoundConstructor
+	public PretestCommitPreCheckout() {
+	}
+
+	/**
+	 * Overridden setup returns a noop class as we don't want to add annything
+	 * here.
+	 *
+	 * @param build
+	 * @param launcher
+	 * @param listener
+	 * @return noop Environment class
+	 */
+	@Override
+	public Environment setUp(AbstractBuild build, Launcher launcher,
+			BuildListener listener) throws IOException, InterruptedException {
+		listener.getLogger().println("Setup!!!");
+		return new NoopEnv();
+	}
+	
+	/**
+	 * Overridden precheckout step, this is where wedo all the work.
+	 *
+	 * Checks to make sure we have some buildsteps set,
+	 * and then calls the prebuild and perform on all of them.
+	 * @todo handle build steps failure in some sort of reasonable way
+	 *
+	 * @param build
+	 * @param launcher
+	 * @param listener
+	 */
+	@Override
+	public void preCheckout(AbstractBuild build, Launcher launcher,
+			BuildListener listener) throws IOException, InterruptedException {
+		//PrintStream log = listener.getLogger();
+		
+		listener.getLogger().println("Pre-checkout!!!");
+	}
+	
+	@Extension
+	public static final class DescriptorImpl extends Descriptor<BuildWrapper> {
+		public String getDisplayName() {
+			return "Run pretest-commit stuff before SCM runs";
+		}
+	}
+	
+	class NoopEnv extends Environment {
+	}
+}
