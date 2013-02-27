83f4b44b3b20f6a75528cbf4411eed5aa9e39b7c
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-02-27 16:07:34 +0100

    Fixed whitespace in HelloWorldBuilder.java

--- src/main/java/org/jenkinsci/plugins/pretest_commit/HelloWorldBuilder.java
+++ src/main/java/org/jenkinsci/plugins/pretest_commit/HelloWorldBuilder.java
@@ -35,142 +35,146 @@ import java.io.IOException;
  */
 public class HelloWorldBuilder extends Builder {
 
-    private final String name;
+	private final String name;
 
-    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
-    @DataBoundConstructor
-    public HelloWorldBuilder(String name) {
-        this.name = name;
-    }
+	// Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+	@DataBoundConstructor
+	public HelloWorldBuilder(String name) {
+			this.name = name;
+	}
 
-    /**
-     * We'll use this from the <tt>config.jelly</tt>.
-     */
-    public String getName() {
-        return name;
-    }
+	/**
+	 * We'll use this from the <tt>config.jelly</tt>.
+	 */
+	public String getName() {
+			return name;
+	}
 
-    @Override
-    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        // This is where you 'build' the project.
-        // Since this is a dummy, we just say 'hello world' and call that a build.
+	@Override
+	public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
+			// This is where you 'build' the project.
+			// Since this is a dummy, we just say 'hello world' and call that a build.
 
-        // This also shows how you can consult the global configuration of the builder
-        listener.getLogger().println("Hello, "+name+"!");
-        listener.getLogger().println("Pretested commits enabled?: "+getDescriptor().getPretestCommits());
-        return true;
-    }
+			// This also shows how you can consult the global configuration of the builder
+			listener.getLogger().println("Hello, "+name+"!");
+			listener.getLogger().println("Pretested commits enabled?: "+getDescriptor().getPretestCommits());
+			return true;
+	}
 
-    // Overridden for better type safety.
-    // If your plugin doesn't really define any property on Descriptor,
-    // you don't have to do this.
-    @Override
-    public DescriptorImpl getDescriptor() {
-        return (DescriptorImpl)super.getDescriptor();
-    }
+	// Overridden for better type safety.
+	// If your plugin doesn't really define any property on Descriptor,
+	// you don't have to do this.
+	@Override
+	public DescriptorImpl getDescriptor() {
+			return (DescriptorImpl)super.getDescriptor();
+	}
 
-    /**
-     * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
-     * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
-     */
-    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
-    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
-        /**
-         * To persist global configuration information,
-         * simply store it in a field and call save().
-         *
-         * <p>
-         * If you don't want fields to be persisted, use <tt>transient</tt>.
-         */
-        private boolean pretestCommits;
-        private String textField;
-        private String selectBoxOption;
+	/**
+	 * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
+	 * The class is marked as public so that it can be accessed from views.
+	 *
+	 * <p>
+	 * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+	 * for the actual HTML fragment for the configuration screen.
+	 */
+	@Extension // This indicates to Jenkins that this is an implementation of an extension point.
+	public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+		/**
+		 * To persist global configuration information,
+		 * simply store it in a field and call save().
+		 *
+		 * <p>
+		 * If you don't want fields to be persisted, use <tt>transient</tt>.
+		 */
+		private boolean pretestCommits;
+		private String textField;
+		private String selectBoxOption;
+		
+		/**
+		 * Performs on-the-fly validation of the form field 'name'.
+		 *
+		 * @param value
+		 *			This parameter receives the value that the user has typed.
+		 * @return
+		 *			Indicates the outcome of the validation. This is sent to the browser.
+		 */
+		public FormValidation doCheckName(@QueryParameter String value) 
+				throws IOException, ServletException {
+			if (value.length() == 0) {
+				return FormValidation.error("Please set a name");
+			}
+			if (value.length() < 4) {
+				return FormValidation.warning("Isn't the name too short?");
+			}
+			return FormValidation.ok();
+		}
+		
+		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+			// Indicates that this builder can be used with all kinds of project type
+			return true;
+		}
+		
+		/**
+		 * This human readable name is used in the configuration screen.
+		 */
+		public String getDisplayName() {
+			return "Pre-test commits";
+		}
+		
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject formData) 
+				throws FormException {
+			// To persist global configuration information,
+			// set that to properties and call save().
+			pretestCommits = formData.getBoolean("pretestCommits");
+			textField = formData.getString("textField");
+			selectBoxOption = formData.getString("selectBox");
+			
+			// ^Can also use req.bindJSON(this, formData);
+			//	(easier when there are many fields; need set* methods for this, like setUseFrench)
+			save();
+			return super.configure(req,formData);
+		}
 
-        /**
-         * Performs on-the-fly validation of the form field 'name'.
-         *
-         * @param value
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
-         */
-        public FormValidation doCheckName(@QueryParameter String value)
-                throws IOException, ServletException {
-            if (value.length() == 0)
-                return FormValidation.error("Please set a name");
-            if (value.length() < 4)
-                return FormValidation.warning("Isn't the name too short?");
-            return FormValidation.ok();
-        }
+		/**
+		 * This method returns true if the global configuration says we should speak French.
+		 *
+		 * The method name is bit awkward because global.jelly calls this method to determine
+		 * the initial state of the checkbox by the naming convention.
+		 */
+		
+		public boolean getPretestCommits() {
+			return pretestCommits;
+		}
+		
+		public String getTextField() {
+			return textField;
+		}
+		
+		public String getSelectBox(){
+			return selectBoxOption;
+		}
+		
+		public ListBoxModel doFillSelectBoxItems() {
+			ListBoxModel items = new ListBoxModel();
+		
+			items.add("An option provided by the descriptor","1");
+			items.add("Another option","2");
+		
+		return items;
+		}
 
-        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types 
-            return true;
-        }
-
-        /**
-         * This human readable name is used in the configuration screen.
-         */
-        public String getDisplayName() {
-            return "Pre-test commits";
-        }
-
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            // To persist global configuration information,
-            // set that to properties and call save().
-            pretestCommits = formData.getBoolean("pretestCommits");
-            textField = formData.getString("textField");
-            selectBoxOption = formData.getString("selectBox");
-
-            // ^Can also use req.bindJSON(this, formData);
-            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
-            save();
-            return super.configure(req,formData);
-        }
-
-        /**
-         * This method returns true if the global configuration says we should speak French.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
-         */
-       public boolean getPretestCommits() {
-         return pretestCommits;
-       }
-
-       public String getTextField() {
-         return textField;
-       }
-
-       public String getSelectBox(){
-         return selectBoxOption;
-       }
-
-      public ListBoxModel doFillSelectBoxItems() {
-        ListBoxModel items = new ListBoxModel();
-
-        items.add("An option provided by the descriptor","1");
-        items.add("Another option","2");
-
-        return items;
-      }
-
-      public FormValidation doTestTextField(@QueryParameter("textField")
-      final String textField) {
-        try {
-          if(textField.length() == 4) {
-						return FormValidation.ok("Success!");
-          } else {
-						return FormValidation.error("Please input exactly 4 characters");
-          }
-        } catch (Exception e) {
-          return FormValidation.error("Some unkown error occured");
-        }
-      }
-    }
+		public FormValidation doTestTextField(@QueryParameter("textField")
+				final String textField) {
+			try {
+				if(textField.length() == 4) {
+					return FormValidation.ok("Success!");
+				} else {
+					return FormValidation.error("Please input exactly 4 characters");
+				}
+			} catch (Exception e) {
+				return FormValidation.error("Some unkown error occured");
+			}
+		}
+	}
 }
