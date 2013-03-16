a1d25e3bc5f7b3986682bc8826035cbc5e23088c
Esben Skaarup <sben@sben.dk>
2013-03-16 18:11:01 +0100

    Indentation.
    --HG--
    extra : convert_revision : 133a52077a1ccb3f36b4defa9da75cfe039d48b6

--- src/main/java/org/jenkinsci/plugins/pretestcommit/PretestCommitStatus.java
+++ src/main/java/org/jenkinsci/plugins/pretestcommit/PretestCommitStatus.java
@@ -38,119 +38,129 @@ import com.google.common.collect.Lists;
  */
 @Extension
 public class PretestCommitStatus implements RootAction {
-
+	
 	/* (non-Javadoc)
 	 * @see hudson.model.Action#getDisplayName()
 	 */
-
 	public String getDisplayName() {
 		return "Pretest";
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see hudson.model.Action#getIconFileName()
 	 */
-
 	public String getIconFileName() {
 		return null;
 	}
-
+	
 	/* (non-Javadoc)
 	 * @see hudson.model.Action#getUrlName()
 	 */
-
 	public String getUrlName() {
 		return "pretest-commit";
 	}
-    
-    private static int getPort(URI uri) {
-        int port = uri.getPort();
-        if ( port < 0 ){
-            String scheme = uri.getScheme();
-            if ( scheme.equals("http") ){
-                port = 80;
-            } else if ( scheme.equals("https") ) {
-                port = 443;
-            } else if ( scheme.equals("ssh") ) {
-                port = 22;
-            }
-        }
-        return port;
-    }
-    
-    static boolean looselyMatches(URI notifyUri, String repository) {
-        boolean result = false;
-        try {
-            URI repositoryUri = new URI(repository);
-            result = Objects.equal(notifyUri.getScheme(), repositoryUri.getScheme())
-                && Objects.equal(notifyUri.getHost(), repositoryUri.getHost())
-                && getPort(notifyUri) == getPort(repositoryUri)
-                && Objects.equal(notifyUri.getPath(), repositoryUri.getPath())
-                && Objects.equal(notifyUri.getQuery(), repositoryUri.getQuery());
-        } catch ( URISyntaxException ex ) {
-            LOGGER.log(Level.SEVERE, "could not parse repository uri " + repository, ex);
-        }
-        return result;
-    }
-
-    public HttpResponse doNotifyCommit(@QueryParameter(required=true) final String url) throws ServletException, IOException {
-        // run in high privilege to see all the projects anonymous users don't see.
-        // this is safe because we only initiate polling.
-        SecurityContext securityContext = ACL.impersonate(ACL.SYSTEM);
-        try {
-            return handleNotifyCommit(new URI(url));
-        } catch ( URISyntaxException ex ) {
-            throw HttpResponses.error(SC_BAD_REQUEST, ex);
-        } finally {
-            SecurityContextHolder.setContext(securityContext);
-        }
-    }
-    
-    private HttpResponse handleNotifyCommit(URI url) throws ServletException, IOException {
-        final List<AbstractProject<?,?>> projects = Lists.newArrayList();
-        boolean scmFound = false,
-                triggerFound = false,
-                urlFound = false;
-        for (AbstractProject<?,?> project : Hudson.getInstance().getAllItems(AbstractProject.class)) {
-            SCM scm = project.getScm();
-            if (scm instanceof MercurialSCM) scmFound = true; else continue;
-
-            MercurialSCM hg = (MercurialSCM) scm;
-            String repository = hg.getSource();
-            if (looselyMatches(url, repository)) urlFound = true; else continue;
-            SCMTrigger trigger = project.getTrigger(SCMTrigger.class);
-            if (trigger!=null) triggerFound = true; else continue;
-
-            LOGGER.log(Level.INFO, "Triggering the polling of {0}", project.getFullDisplayName());
-            trigger.run();
-            projects.add(project);
-        }
-
-        final String msg;
-        if (!scmFound) msg = "No mercurial jobs found";
-        else if (!urlFound) msg = "No mercurial jobs using repository: " + url;
-        else if (!triggerFound) msg = "Jobs found but they aren't configured for polling";
-        else msg = null;
-
-        return new HttpResponse() {
-            public void generateResponse(StaplerRequest req, StaplerResponse rsp, Object node) throws IOException, ServletException {
-                rsp.setStatus(SC_OK);
-                rsp.setContentType("text/plain");
-                for (AbstractProject<?, ?> p : projects) {
-                    rsp.addHeader("Triggered", p.getAbsoluteUrl());
-                }
-                PrintWriter w = rsp.getWriter();
-                for (AbstractProject<?, ?> p : projects) {
-                    w.println("Scheduled polling of "+p.getFullDisplayName());
-                }
-                if (msg!=null)
-                    w.println(msg);
-            }
-        };
-    }
-
-    private static final Logger LOGGER = Logger.getLogger(PretestCommitStatus.class.getName());
-    /*
+	
+	private static int getPort(URI uri) {
+		int port = uri.getPort();
+		if ( port < 0 ){
+			String scheme = uri.getScheme();
+			if ( scheme.equals("http") ){
+				port = 80;
+			} else if ( scheme.equals("https") ) {
+				port = 443;
+			} else if ( scheme.equals("ssh") ) {
+				port = 22;
+			}
+		}
+		return port;
+	}
+	
+	static boolean looselyMatches(URI notifyUri, String repository) {
+		boolean result = false;
+		try {
+			URI repositoryUri = new URI(repository);
+			result = Objects.equal(notifyUri.getScheme(),
+				repositoryUri.getScheme())
+				&& Objects.equal(notifyUri.getHost(), repositoryUri.getHost())
+				&& getPort(notifyUri) == getPort(repositoryUri)
+				&& Objects.equal(notifyUri.getPath(), repositoryUri.getPath())
+				&& Objects.equal(notifyUri.getQuery(),
+				repositoryUri.getQuery());
+		} catch ( URISyntaxException ex ) {
+			LOGGER.log(Level.SEVERE, "could not parse repository uri "
+					+ repository, ex);
+		}
+		return result;
+	}
+	
+	public HttpResponse doNotifyCommit(
+			@QueryParameter(required=true) final String url)
+			throws ServletException, IOException {
+		// run in high privilege to see all the projects anonymous users don't
+		// see.
+		// this is safe because we only initiate polling.
+		SecurityContext securityContext = ACL.impersonate(ACL.SYSTEM);
+		try {
+			return handleNotifyCommit(new URI(url));
+		} catch ( URISyntaxException ex ) {
+			throw HttpResponses.error(SC_BAD_REQUEST, ex);
+		} finally {
+			SecurityContextHolder.setContext(securityContext);
+		}
+	}
+	
+	private HttpResponse handleNotifyCommit(URI url)
+			throws ServletException, IOException {
+		final List<AbstractProject<?,?>> projects = Lists.newArrayList();
+		boolean scmFound = false,
+				triggerFound = false,
+				urlFound = false;
+		for (AbstractProject<?,?> project
+				: Hudson.getInstance().getAllItems(AbstractProject.class)) {
+			SCM scm = project.getScm();
+			if (scm instanceof MercurialSCM) scmFound = true; else continue;
+			
+			MercurialSCM hg = (MercurialSCM) scm;
+			String repository = hg.getSource();
+			if (looselyMatches(url, repository)) urlFound = true; else continue;
+			SCMTrigger trigger = project.getTrigger(SCMTrigger.class);
+			if (trigger != null) triggerFound = true; else continue;
+			
+			LOGGER.log(Level.INFO, "Triggering the polling of {0}",
+					project.getFullDisplayName());
+			trigger.run();
+			projects.add(project);
+		}
+		
+		final String msg;
+		if (!scmFound) msg = "No mercurial jobs found";
+		else if (!urlFound) msg = "No mercurial jobs using repository: " + url;
+		else if (!triggerFound) msg =
+				"Jobs found but they aren't configured for polling";
+		else msg = null;
+		
+		return new HttpResponse() {
+			public void generateResponse(StaplerRequest req,
+					StaplerResponse rsp, Object node)
+					throws IOException, ServletException {
+				rsp.setStatus(SC_OK);
+				rsp.setContentType("text/plain");
+				for (AbstractProject<?, ?> p : projects) {
+					rsp.addHeader("Triggered", p.getAbsoluteUrl());
+				}
+				PrintWriter w = rsp.getWriter();
+				for (AbstractProject<?, ?> p : projects) {
+					w.println("Scheduled polling of "+p.getFullDisplayName());
+				}
+				if (msg!=null)
+					w.println(msg);
+			}
+		};
+	}
+	
+	private static final Logger LOGGER =
+			Logger.getLogger(PretestCommitStatus.class.getName());
+	/*
 	public HttpResponse doFoo(){
 		HttpResponse response = new HttpResponse() {
 			public void generateResponse(StaplerRequest req, StaplerResponse rsp, Object node) throws IOException, ServletException {
@@ -169,23 +179,23 @@ public class PretestCommitStatus implements RootAction {
 					msg = sb.toString();
 					
 					final List<AbstractProject<?,?>> projects = Lists.newArrayList();
-			        boolean scmFound = false,
-			                triggerFound = false,
-			                urlFound = false;
-			        for (AbstractProject<?,?> project : Hudson.getInstance().getAllItems(AbstractProject.class)) {
-			            SCM scm = project.getScm();
-			            if (scm instanceof MercurialSCM) scmFound = true; else continue;
+					boolean scmFound = false,
+							triggerFound = false,
+							urlFound = false;
+					for (AbstractProject<?,?> project : Hudson.getInstance().getAllItems(AbstractProject.class)) {
+						SCM scm = project.getScm();
+						if (scm instanceof MercurialSCM) scmFound = true; else continue;
 
-			            MercurialSCM hg = (MercurialSCM) scm;
-			            String repository = hg.getSource();
-			            if (looselyMatches(url, repository)) urlFound = true; else continue;
-			            SCMTrigger trigger = project.getTrigger(SCMTrigger.class);
-			            if (trigger!=null) triggerFound = true; else continue;
+						MercurialSCM hg = (MercurialSCM) scm;
+						String repository = hg.getSource();
+						if (looselyMatches(url, repository)) urlFound = true; else continue;
+						SCMTrigger trigger = project.getTrigger(SCMTrigger.class);
+						if (trigger!=null) triggerFound = true; else continue;
 
-			            LOGGER.log(Level.INFO, "Triggering the polling of {0}", project.getFullDisplayName());
-			            trigger.run();
-			            projects.add(project);
-			        }
+						LOGGER.log(Level.INFO, "Triggering the polling of {0}", project.getFullDisplayName());
+						trigger.run();
+						projects.add(project);
+					}
 					
 				} catch (Exception e) {
 					//Catch the exception
@@ -193,18 +203,18 @@ public class PretestCommitStatus implements RootAction {
 				}
 				
 				rsp.setStatus(SC_OK);
-                rsp.setContentType("text/plain");
-                //for (AbstractProject<?, ?> p : projects) {
-                //    rsp.addHeader("Triggered", p.getAbsoluteUrl());
-                //}
-                
-                PrintWriter w = rsp.getWriter();
-                //for (AbstractProject<?, ?> p : projects) {
-                //    w.println("Scheduled polling of "+p.getFullDisplayName());
-                //}
-                
-                if (msg!=null)
-                    w.println(msg);
+				rsp.setContentType("text/plain");
+				//for (AbstractProject<?, ?> p : projects) {
+				//	rsp.addHeader("Triggered", p.getAbsoluteUrl());
+				//}
+				
+				PrintWriter w = rsp.getWriter();
+				//for (AbstractProject<?, ?> p : projects) {
+				//	w.println("Scheduled polling of "+p.getFullDisplayName());
+				//}
+				
+				if (msg!=null)
+					w.println(msg);
 			}
 		};
 		return response;
