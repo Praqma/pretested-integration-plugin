df306210bac4fef8e4554e41944df36d3b086fcf
Alexander Uldall <morpmex@gmail.com>
2013-03-28 17:31:43 +0100

    Made changes to the python changegroup hook

--- resources/hg_change_group_hook.py
+++ resources/hg_change_group_hook.py
@@ -5,19 +5,22 @@ from urllib import urlencode
 import os
 
 def run(ui, repo, **kwargs):
-	branch = repo[None].branch()
-	changeset = repo.changelog.tip()
-	user = repo[None].user()
+	user_branch = repo[None].branch()
+	user_changeset = repo.changelog.tip()
+	user_name = repo[None].user()
+	user_repository_url = os.getcwd()
 	#rev = repo[None].rev()
 	#ui.warn(dir(repo[None]) ) 
-	ui.warn('branch name: %s \n' %(str(branch)))
-	ui.warn('changeset: %s \n' %(hex(changeset)))
-	ui.warn('user: %s \n' %(user))
+	ui.warn('branch name: %s \n' %(str(user_branch)))
+	ui.warn('changeset: %s \n' %(hex(user_changeset)))
+	ui.warn('user: %s \n' %(user_name))
+	ui.warn('repo_rul: %s \n' %(user_repository_url))
 	#ui.warn('rev: %s \n' %(str(rev)))
 	http = HTTPConnection('localhost')
-	data = {branch:str(branch), changeset:hex(changeset),user:user}
-	ui.warn("http://localhost"+urlencode(data))
+	data = {"user_branch":str(user_branch), "user_changeset":hex(user_changeset),"user_name":user_name,"user_repository_url":"file://"+user_repository_url }
+	ui.warn("http://localhost"+urlencode(data)+'\n')
 	http.request("GET","http://localhost"+urlencode(data)+'\n')
 	ui.warn(str(http.getresponse()))
 	return True
+
 
