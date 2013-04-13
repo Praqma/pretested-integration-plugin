99ffda8e862f4b0bd20161e8f779e5f2c16aa5db
Esben Skaarup <sben@sben.dk>
2013-04-13 13:29:14 +0200

    Changed parameters to be retrieved from hg log.

--- src/main/java/org/jenkinsci/plugins/pretestcommit/PretestCommitPreCheckout.java
+++ src/main/java/org/jenkinsci/plugins/pretestcommit/PretestCommitPreCheckout.java
@@ -4,6 +4,8 @@ import hudson.AbortException;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.Launcher;
+import hudson.Launcher.ProcStarter;
+import hudson.Proc;
 import hudson.model.BuildListener;
 import hudson.model.Environment;
 import hudson.model.AbstractBuild;
@@ -26,6 +28,9 @@ import hudson.FilePath;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -149,14 +154,84 @@ public class PretestCommitPreCheckout extends BuildWrapper {
 	public Environment setUp(AbstractBuild build, Launcher launcher,
 			BuildListener listener) throws IOException, InterruptedException {
 		
-		Map<String,String> vars = build.getBuildVariables();
-		String repositoryURL = vars.get("user_repository_url").toString();
-		String branch = vars.get("user_branch").toString();
-		String changeset = vars.get("user_changeset").toString();
-		String user = vars.get("user_name").toString();
+		ArgumentListBuilder cmd = createArgumentListBuilder(
+				build, launcher, listener);
+		cmd.add("log");
+		cmd.add("-l");
+		cmd.add("1");
+		
+		BufferedReader logStdout;
+		int logExitCode;
+		try {
+			Launcher.ProcStarter starter = launcher.launch().cmds(cmd)
+					.pwd(build.getWorkspace()).readStdout();
+			Proc proc = starter.start();
+			logStdout = new BufferedReader(
+					new InputStreamReader(proc.getStdout()));
+			logExitCode = starter.join();
+		} catch(IOException e) {
+			String message = e.getMessage();
+			if (message != null
+					&& message.startsWith("Cannot run program")
+					&& message.endsWith("No such file or directory")) {
+				listener.error("Failed to get hg log"
+						+ " because hg could not be found;"
+						+ " check that you've properly configured your"
+						+ " Mercurial installation");
+			} else {
+				e.printStackTrace(listener.error(
+						"Failed to get hg log"));
+			}
+			throw new AbortException("Failed to get hg log");
+		}
+		if(logExitCode != 0) {
+			listener.error("Failed to get hg log");
+			throw new AbortException("Failed to get hg log");
+		} else {
+			listener.getLogger().println("Successfully got log");
+		}
+		
+		String branch = null;
+		String changeset = null;
+		String user = null;
+		String message = null;
+		String date = null;
+		
+		String line;
+		while((line = logStdout.readLine()) != null) {
+			String firstWord = line.split("\\s+")[0];
+			String restOfLine = line.substring(firstWord.length()).trim();
+			listener.getLogger().println("log line: start = " + firstWord
+					+ ", rest = " + restOfLine);
+			if(firstWord.equals("changeset:")) {
+				changeset = restOfLine;
+			} else if(firstWord.equals("branch:")) {
+				branch = restOfLine;
+			} else if(firstWord.equals("user:")) {
+				user = restOfLine;
+			} else if(firstWord.equals("date:")) {
+				date = restOfLine;
+			} else if(firstWord.equals("summary:")) {
+				message = restOfLine;
+			}
+		}
+		
+		listener.getLogger().println("changeset: " + changeset);
+		listener.getLogger().println("branch: " + branch);
+		listener.getLogger().println("user: " + user);
+		listener.getLogger().println("date: " + date);
+		listener.getLogger().println("message: " + message);
+		
+		// Map<String,String> vars = build.getBuildVariables();
+		// String repositoryURL = vars.get("user_repository_url").toString();
+		// String branch = vars.get("user_branch").toString();
+		// String changeset = vars.get("user_changeset").toString();
+		// String user = vars.get("user_name").toString();
+		
+		
 //		test build URL: sben.dk:8081/job/Demo job/buildWithParameters?user_name=mig&user_changeset=1234&user_branch=b&user_repository_url=file:///home/nogen/hej
-		mergeWithNewBranch(build, launcher, listener, repositoryURL, branch,
-				changeset, user);
+//		mergeWithNewBranch(build, launcher, listener, repositoryURL, branch,
+//				changeset, user);//TODO find repositoryURL
 		
 		return new NoopEnv();
 		
