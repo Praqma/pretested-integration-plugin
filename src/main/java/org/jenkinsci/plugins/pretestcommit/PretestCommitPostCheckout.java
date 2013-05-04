79450cb20f540d580bfd89297ba64afd5f331b52
Alexander Uldall <morpmex@gmail.com>
2013-05-04 15:36:36 +0200

    commited to pull
    --HG--
    extra : convert_revision : bab1e41404e124cf5f5450d5b1d38dd9edc8ccc6

--- src/main/java/org/jenkinsci/plugins/pretestcommit/PretestCommitPostCheckout.java
+++ src/main/java/org/jenkinsci/plugins/pretestcommit/PretestCommitPostCheckout.java
@@ -77,17 +77,8 @@ public class PretestCommitPostCheckout extends Publisher {
 		String sourceBranch = newCommitInfo.get("branch");
 		PretestUtils.logMessage(listener, "commit is on this branch: "
 				+ sourceBranch);
-
 		HgUtils.runScmCommand(build, launcher, listener,
-				new String[]{"up", "default"});
-		HgUtils.runScmCommand(build, launcher, listener,
-				new String[]{"merge", sourceBranch});
-		HgUtils.runScmCommand(build, launcher, listener,
-				new String[]{"commit", "-m", "dummy commit"});
-		HgUtils.runScmCommand(build, launcher, listener,
-				new String[]{"push", "--branch", "default"});
-		//HgUtils.runScmCommand(build, launcher, listener,
-		//		new String[]{"push", "--branch", sourceBranch});
+				new String[]{"push", "--branch", sourceBranch});
 	}
 	
 	/**
