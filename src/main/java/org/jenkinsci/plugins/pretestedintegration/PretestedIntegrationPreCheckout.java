62ac00996acb1e3f62d4a3d8ade6446b117e15b3
Esben Skaarup <sben@sben.dk>
2013-06-02 14:49:11 +0200

    Modified the pre-checkout class to work with scm plugin plugins.
    --HG--
    extra : convert_revision : 12ae3b88e0997cba3b6d1530780fed550aeaffb5

--- src/main/java/org/jenkinsci/plugins/pretestedintegration/PretestedIntegrationPreCheckout.java
+++ src/main/java/org/jenkinsci/plugins/pretestedintegration/PretestedIntegrationPreCheckout.java
@@ -21,6 +21,10 @@ import javax.servlet.ServletException;
 
 import net.sf.json.JSONObject;
 import org.jenkinsci.plugins.pretestedintegration.CommitQueue;
+import org.jenkinsci.plugins.pretestedintegration.scminterface
+		.PretestedIntegrationSCMInterface;
+import org.jenkinsci.plugins.pretestedintegration.scminterface
+		.AvailableInterfaces;
 
 import org.ini4j.Ini;
 import org.ini4j.InvalidFileFormatException;
@@ -37,67 +41,21 @@ public class PretestedIntegrationPreCheckout extends BuildWrapper {
 	private static final String DISPLAY_NAME = "Use pretested integration";
 	private static final String PLUGIN_NAME = "pretested-integration";
 	
-	private final String stageRepositoryUrl;
-
 	private boolean hasQueue;
 	
-	@DataBoundConstructor
-	public PretestedIntegrationPreCheckout(String stageRepositoryUrl) {
-		this.stageRepositoryUrl = stageRepositoryUrl;
-	}
-
-	public String getStageRepositoryUrl() {
-		return stageRepositoryUrl;
-	}
-	
-	private String getScmRepositoryURL(AbstractBuild build, Launcher launcher,
+	private String getScmType(AbstractBuild build, Launcher launcher,
 			BuildListener listener) throws AbortException {
-		//Setup variables to find our executable
 		AbstractProject<?,?> project = build.getProject();
-		//We need to check this cast..
-		MercurialSCM scm = null;
-		try {
-			scm = (MercurialSCM) project.getScm();
-		} catch(ClassCastException e) {
-			listener.error("The chosen SCM is not Mercurial!");
-			throw new AbortException("The chosen SCM is not Mercurial!");
-		}
-		return scm.getSource();
+		return project.getScm().getType();
 	}
 	
-	void mergeWithNewBranch(AbstractBuild build, Launcher launcher,
-			BuildListener listener, String repositoryURL)
-			throws IOException, InterruptedException {
-		
-		PretestUtils.logMessage(listener, "Pulling changes from stage");
-		//First get the curent tip info
-		Dictionary<String, String> oldVars = HgUtils.getNewestCommitInfo(build, launcher, listener);
-		String oldTip = oldVars.get("changeset");
-		Dictionary<String, String> newVars = null;
-		
-		try {
-			HgUtils.runScmCommand(build, launcher, listener, 
-					new String[]{"update","default"});
-			HgUtils.runScmCommand(build, launcher, listener,
-				new String[]{"pull", "--update", repositoryURL});
-			
-			
-			newVars =  HgUtils.getNewestCommitInfo(build, launcher, listener);
-			
-			HgUtils.runScmCommand(build, launcher, listener,
-				new String[]{"merge", newVars.get("branch"),"--tool","internal:merge"});
-			//HgUtils.runScmCommand(build, launcher, listener,
-			//	new String[]{"commit", "-m", newVars.get("message")});
-		} catch(AbortException e)
-		{
-			PretestUtils.logError(listener, "Could not merge with branch: "+newVars.get("branch"));
-			throw e;
-		}
+	@DataBoundConstructor
+	public PretestedIntegrationPreCheckout() {
 	}
 	
 	/**
 	 * Jenkins hook that fires after the workspace is initialized.
-	 *
+	 * 
 	 * @param build
 	 * @param launcher
 	 * @param listener
@@ -108,19 +66,55 @@ public class PretestedIntegrationPreCheckout extends BuildWrapper {
 			BuildListener listener) throws IOException, InterruptedException {
 		PretestUtils.logMessage(listener, "Beginning pre-build step");
 		
-		// Get info about the newest commit and store it in the environment.
-		// This is used to determine the triggering build, even if newer commits
-		// are applied while this job is in the queue.
-		Environment environment = new PretestEnvironment();
-		
 		// Wait in line until no other jobs are running.
 		CommitQueue.getInstance().enqueueAndWait();
 		hasQueue = true;
 		
-		mergeWithNewBranch(build,launcher, listener, stageRepositoryUrl);
+		// Get the interface for the SCM according to the chosen SCM
+		String scmType = getScmType(build, launcher, listener);
+		if(scmType == null) {
+			PretestUtils.logMessage(
+					listener, "No SCM chosen");
+			return null;
+		}
+		Class scmClass = AvailableInterfaces.getClassByName(scmType);
+		if(scmClass == null) {
+			PretestUtils.logMessage(
+					listener, "No interface found for SCM type: " + scmType);
+			return null;
+		}
+		PretestedIntegrationSCMInterface scmInterface;
+		try {
+			scmInterface = (PretestedIntegrationSCMInterface)
+					scmClass.newInstance();
+		} catch(InstantiationException e) {
+			PretestUtils.logMessage(listener, "Could not instantiate class: "
+					+ AvailableInterfaces.getClassByName(scmType));
+			return null;
+		} catch(IllegalAccessException e) {
+			PretestUtils.logMessage(listener, "Could not instantiate class: "
+					+ AvailableInterfaces.getClassByName(scmType));
+			return null;
+		} catch(ClassCastException e) {
+			PretestUtils.logMessage(listener, "SCM interface class does not"
+					+ " implement the PretestedIntegrationSCMInterface: "
+					+ AvailableInterfaces.getClassByName(scmType));
+			return null;
+		}
+		
+		// Verify that there is anything to do
+		if(!scmInterface.hasNextCommit(build, launcher, listener)) {
+			PretestUtils.logMessage(listener, "Nothing to build. Aborting.");
+			return null;
+		}
+		
+		// Prepare for build
+		scmInterface.prepareWorkspace(build, launcher, listener,
+				scmInterface.popCommit(build, launcher, listener));
 		
 		PretestUtils.logMessage(listener, "Finished pre-build step");
 		
+		Environment environment = new PretestEnvironment();
 		return environment;
 	}
 	
@@ -133,12 +127,12 @@ public class PretestedIntegrationPreCheckout extends BuildWrapper {
 	public void preCheckout(AbstractBuild build, Launcher launcher,
 			BuildListener listener) throws IOException, InterruptedException {
 		if(Hudson.getInstance().getPlugin(PLUGIN_NAME) != null) {
-			PretestUtils.logMessage(listener,PLUGIN_NAME+" plugin version: "
+			PretestUtils.logMessage(listener, PLUGIN_NAME + " plugin version: "
 					+ Hudson.getInstance().getPlugin(PLUGIN_NAME)
 					.getWrapper().getVersion());
-		}else{
-		PretestUtils.logMessage(listener, "No plugin found with name "
-				+ PLUGIN_NAME);
+		} else {
+			PretestUtils.logMessage(listener, "No plugin found with name "
+					+ PLUGIN_NAME);
 		}
 	}
 	
@@ -154,147 +148,6 @@ public class PretestedIntegrationPreCheckout extends BuildWrapper {
 			return DISPLAY_NAME;
 		}
 		
-		/**
-		 * Gets the configuration directory for a repository
-		 * @param repositoryUrl
-		 * @return
-		 */
-		public File configurationDirectory(final String repositoryUrl){
-			return new File(repositoryUrl, ".hg");
-		}
-		
-		/**
-		 * Tests whether a repository exists at the specified location
-		 * @assert That repositoryUrl is a local repository the user has access
-		 * to
-		 */
-		public boolean validateConfiguration(final String repositoryUrl) {
-			File repoDir = configurationDirectory(repositoryUrl);
-			//We should check whether the hgrc file is correctly configured 
-			//and that the python hook file is properly set
-			//That the repository exists is good enough for now
-			return repoDir.exists();
-		}
-		
-		public boolean setupRepositoryDirectory(File repoDir) {
-			return (repoDir.exists() || repoDir.mkdirs());
-		}
-		
-		/**
-		 * Updates the configuration file in the specified repository
-		 * @param repositoryUrl
-		 */
-		public boolean writeConfiguration(final String repositoryUrl) {
-			File repoDir = configurationDirectory(repositoryUrl);
-			if(setupRepositoryDirectory(repoDir)){
-				//the hgDir now is a valid path
-				//Write the hgrc file
-				try {
-					File hgrc = new File(repoDir,"hgrc");
-					if(hgrc.canWrite() || hgrc.createNewFile()){
-						Ini ini;
-						if(File.separatorChar == '\\') {
-							ini = new Wini(hgrc);
-						} else {
-							ini = new Ini(hgrc);
-						}
-						ini.put("hooks","pretxnchangegroup", 
-							"python:.hg/hg_changegroup_hook.py:run");
-						ini.store();
-						return true;
-					} else {
-						return false;
-					}
-				} catch(InvalidFileFormatException e) {
-					System.out.println("Invalid file format");
-				} catch(IOException e) {	
-					System.out.println("Some ioxception occured");
-				}
-			}
-			return false;
-		}
-		
-		public String getJenkinsRootUrl() {
-			return Hudson.getInstance()
-					.getRootUrl()
-					.replaceAll("^http://|/$", "");
-		}
-		
-		/**
-		 * Updates the hook file in the specified repository
-		 * @param repositoryUrl
-		 * @return 
-		 */
-		public boolean writeHook(final File repoDir, final String jenkinsRoot, final String job) throws IOException {
-			File hook = new File(repoDir,"hg_changegroup_hook.py");
-			if(hook.canWrite() || hook.createNewFile()){
-				FileWriter fw = new FileWriter(hook);
-				fw.write("from mercurial import ui, hg\r\n");
-				fw.write("from mercurial.node import hex\r\n");
-				fw.write("from httplib import HTTPConnection\r\n");
-				fw.write("from urllib import urlencode\r\n");
-				fw.write("import os\r\n\r\n");
-						
-				fw.write("def run(ui, repo, **kwargs):\r\n");
-				fw.write("\thttp = HTTPConnection(\"" 
-						+ jenkinsRoot + "\")\r\n");
-				fw.write("\thttp.request(\"GET\",\"http://" 
-						+ jenkinsRoot + "/job/" 
-						+ job + "/build\")\r\n");
-				fw.write("\tui.warn(\"http://" 
-						+ jenkinsRoot + "/job/" 
-						+ job + "/build\\n\")\r\n");
-				fw.write("\tui.warn(str(http.getresponse().read())"
-						+ "+\"\\n\")\r\n");
-				fw.write("\treturn False\r\n");
-						
-				fw.close();
-				return true;
-			}
-			
-			return false;
-		}
-		
-		
-		/**
-		 * Performs a validation test whether the repository url is correctly 
-		 * initialised
-		 */
-		public FormValidation doTestRepository(
-				@QueryParameter("stageRepositoryUrl") final String repositoryUrl) 
-						throws IOException, ServletException {
-			boolean isValid = validateConfiguration(repositoryUrl);
-			if(isValid) {
-				return FormValidation.ok("The path contains a valid mercurial repository");
-			} else {
-				return FormValidation.error("The repository is not a mercurial repository");
-			}
-		}
-		
-		/**
-		 * Updates or creates the repository url to be functional
-		 * @param repositoryUrl
-		 * @return
-		 */
-		public FormValidation doUpdateRepository(@QueryParameter("stageRepositoryUrl") final String repositoryUrl, 
-				@QueryParameter("name") final String name) {
-			boolean updateConfiguration = writeConfiguration(repositoryUrl);
-			if(updateConfiguration){
-				File configDir = configurationDirectory(repositoryUrl);
-				if(setupRepositoryDirectory(configDir)){
-					try {
-						boolean updateHook = writeHook(configDir, getJenkinsRootUrl(), name);
-						if(updateHook) {
-							return FormValidation.ok("Configuration updated");
-						}
-					} catch (IOException e){
-						
-					}
-				}
-			}
-			return FormValidation.error("Configuration could not be updated");
-		}
-
 		@Override
 		public boolean isApplicable(AbstractProject<?, ?> arg0) {
 			// TODO Auto-generated method stub
