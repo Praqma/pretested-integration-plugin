9854df8e32f606b13b79ee062295f62fb637fd05
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-07-02 15:12:46 +0200

    Cleanup up PretestedIntegrationPreCheckout
    Removed outcommented code and made everything nice and tidy
    
    --HG--
    extra : convert_revision : 0a64642376d198e6b68861bda47f1c16f6c1d656

--- src/main/java/org/jenkinsci/plugins/pretestedintegration/PretestedIntegrationPreCheckout.java
+++ src/main/java/org/jenkinsci/plugins/pretestedintegration/PretestedIntegrationPreCheckout.java
@@ -3,29 +3,18 @@ package org.jenkinsci.plugins.pretestedintegration;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.BuildListener;
-import hudson.model.Describable;
 import hudson.model.Result;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Descriptor;
-import hudson.model.Hudson;
 import hudson.tasks.BuildWrapper;
 import hudson.tasks.BuildWrapperDescriptor;
-import hudson.util.ListBoxModel;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
-
-//import org.jenkinsci.plugins.pretestedintegration.scminterface.mercurial.HgUtils;
 
 /**
  * The build wrapper determines what will happen before the build will run.
@@ -33,15 +22,7 @@ import org.kohsuke.stapler.StaplerRequest;
  */
 public class PretestedIntegrationPreCheckout extends BuildWrapper {
 	
-	private static final String DISPLAY_NAME = "Use pretested integration";
-	private static final String PLUGIN_NAME = "pretested-integration";
-	
 	private AbstractSCMInterface scmInterface;
-	
-/*	@DataBoundConstructor
-	public PretestedIntegrationPreCheckout(Map<Descriptor, Describable> instances) {
-		this.instances = instances;
-	}*/
 	
 	@DataBoundConstructor
 	public PretestedIntegrationPreCheckout(AbstractSCMInterface scmInterface){
@@ -51,11 +32,6 @@ public class PretestedIntegrationPreCheckout extends BuildWrapper {
 	public AbstractSCMInterface getScmInterface(){
 		return this.scmInterface;
 	}
-	/*
-	public Map<Descriptor, Describable> getInstances(){
-		return this.instances;
-	}*/
-	
 	
 	/**
 	 * Jenkins hook that fires after the workspace is initialized.
@@ -115,30 +91,12 @@ public class PretestedIntegrationPreCheckout extends BuildWrapper {
 			SCMInterfaceDescriptor<AbstractSCMInterface> d = (SCMInterfaceDescriptor<AbstractSCMInterface>) b.getScmInterface().getDescriptor();
 			b.scmInterface = d.newInstance(req, formData);
 
-//			List<AbstractSCMInterface> is = new ArrayList<AbstractSCMInterface>();
-			/*Map<Descriptor,Describable> instances = new HashMap<Descriptor,Describable>();
-			List<SCMInterfaceDescriptor<?>> descriptors = getSCMs();
-			Iterator<SCMInterfaceDescriptor<?>> it = descriptors.iterator();
-			
-			while(it.hasNext()){
-				SCMInterfaceDescriptor<?> d = it.next();
-				instances.put(d, d.newInstance(req,formData));
-			}
-			b.instances = instances;*/
 			save();
 			return b;
 		}
 
 		public List<SCMInterfaceDescriptor<?>>getSCMInterfaces(){
 			return AbstractSCMInterface.getDescriptors();
-		}
-		
-		public ListBoxModel doFillScmInterfaceItems() {
-			ListBoxModel items = new ListBoxModel();
-			for(Descriptor d : getSCMInterfaces()){
-				items.add(d.getDisplayName(),d.getId());
-			}
-			return items;
 		}
 		
 		@Override
