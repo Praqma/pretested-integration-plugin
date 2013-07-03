c4b5042d556a75bdd547bb6f5a7a50f236855489
Ronni Elken Lindsgaard <rel@it-minds.dk>
2013-07-03 19:17:14 +0200

    Added Mercurial instance testing
    --HG--
    extra : convert_revision : ace9dd240deba10f1c4146cf0bff305e4aeafad0

--- src/test/java/org/jenkinsci/plugins/pretestedintegration/scm/mercurial/MercurialTest.java
+++ src/test/java/org/jenkinsci/plugins/pretestedintegration/scm/mercurial/MercurialTest.java
@@ -1,7 +1,11 @@
 package org.jenkinsci.plugins.pretestedintegration.scm.mercurial;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.jenkinsci.plugins.pretestedintegration.AbstractSCMInterface;
 import org.jenkinsci.plugins.pretestedintegration.SCMInterfaceDescriptor;
+import org.junit.Assert;
 import org.jvnet.hudson.test.HudsonTestCase;
 import static org.mockito.Mockito.*;
 
@@ -14,6 +18,24 @@ public class MercurialTest extends HudsonTestCase {
 				inDescriptorList = true;
 		}
 		assertTrue(inDescriptorList);
+	}
+	
+	public void testShouldInitialise() throws Exception {
+		Mercurial mercurial = new Mercurial(true,"test");
+		assertTrue(mercurial.getReset());
+		assertEquals("test",mercurial.getBranch());
+		
+		mercurial = new Mercurial(false,"test");
+		assertFalse(mercurial.getReset());
+		assertEquals("test",mercurial.getBranch());
+		
+		mercurial = new Mercurial(false,"");
+		assertFalse(mercurial.getReset());
+		assertEquals("default",mercurial.getBranch());
+		
+		mercurial = new Mercurial(false,null);
+		assertFalse(mercurial.getReset());
+		assertEquals("default",mercurial.getBranch());
 	}
 	
 }
