42eb79c29d6b45a44dceea5b13f454816302b9a1
Andrius Ordojan <and@praqma.net>
2014-08-20 10:52:54 +0200

    Added javadoc to the removeRemoteFromBranchName method

--- src/main/java/org/jenkinsci/plugins/pretestedintegration/scm/git/GitUtils.java
+++ src/main/java/org/jenkinsci/plugins/pretestedintegration/scm/git/GitUtils.java
@@ -1,6 +1,13 @@
 package org.jenkinsci.plugins.pretestedintegration.scm.git;
 
 public class GitUtils {
+    /**
+     * Removes the first section of the string.
+     * This method is meant to remove the remote name from a remote branch.
+     *
+     * @param branchName the remote branch
+     * @return If the provided parameter is "origin/ready/feat_1" the method will output "ready/feat_1"
+     */
     public static String removeRemoteFromBranchName(String branchName) {
         if (branchName == null) throw new IllegalArgumentException("String branchName parameter is null");
         if (branchName.isEmpty()) throw new IllegalArgumentException("String branchName parameter is empty");
