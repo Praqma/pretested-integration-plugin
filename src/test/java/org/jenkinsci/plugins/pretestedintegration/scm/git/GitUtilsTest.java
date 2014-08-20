81f4c9705bcb51a62ccedd9b6109b99d6f03afc0
Andrius Ordojan <and@praqma.net>
2014-08-20 12:10:54 +0200

    added more values to test

--- src/test/java/org/jenkinsci/plugins/pretestedintegration/scm/git/GitUtilsTest.java
+++ src/test/java/org/jenkinsci/plugins/pretestedintegration/scm/git/GitUtilsTest.java
@@ -6,13 +6,22 @@ import org.junit.Test;
 public class GitUtilsTest {
     @Test
     public void CanRemoveRemoteFromBranchName() {
-        final String remoteName = "origin";
-        final String expectedResult = "ready/feature1";
-        final String inputBranchName = remoteName + "/" + expectedResult;
+        final String remoteBranch_1 = "origin/ready/dev_myname";
+        final String expectedResult_1 = "ready/dev_myname";
 
-        String result = GitUtils.removeRemoteFromBranchName(inputBranchName);
+        final String remoteBranch_2 = "origin/ready-devename";
+        final String expectedResult_2 = "ready-devename";
 
-        Assert.assertEquals("Remote should be removed from the branch name", expectedResult, result);
+        final String remoteBranch_3 = "myremote/feature-branch";
+        final String expectedResult_3 = "feature-branch";
+
+        final String result_1 = GitUtils.removeRemoteFromBranchName(remoteBranch_1);
+        final String result_2 = GitUtils.removeRemoteFromBranchName(remoteBranch_2);
+        final String result_3 = GitUtils.removeRemoteFromBranchName(remoteBranch_3);
+
+        Assert.assertEquals("Remote should be removed from the branch name", expectedResult_1, result_1);
+        Assert.assertEquals("Remote should be removed from the branch name", expectedResult_2, result_2);
+        Assert.assertEquals("Remote should be removed from the branch name", expectedResult_3, result_3);
     }
 
     @Test(expected = IllegalArgumentException.class)
