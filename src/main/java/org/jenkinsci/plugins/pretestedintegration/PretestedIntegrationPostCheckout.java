a400e3c0895b12602e7b6ce1968d6ca86f818037
Bue Petersen <bue@praqma.net>
2017-09-20 16:43:54 +0200

    For #62 renamed post-build step to explain it is a publisher pushing to integration branch

--- src/main/java/org/jenkinsci/plugins/pretestedintegration/PretestedIntegrationPostCheckout.java
+++ src/main/java/org/jenkinsci/plugins/pretestedintegration/PretestedIntegrationPostCheckout.java
@@ -211,7 +211,7 @@ public class PretestedIntegrationPostCheckout extends Recorder implements Serial
     /**
      * Descriptor Implementation for PretestedIntegrationPostCheckout
      */
-    @Symbol("pretested-integration")
+    @Symbol("PretestedIntegration")
     @Extension(ordinal = Integer.MIN_VALUE)
     public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
@@ -220,7 +220,7 @@ public class PretestedIntegrationPostCheckout extends Recorder implements Serial
          */
         @Override
         public String getDisplayName() {
-            return "Pretested Integration - Update remote repository";
+            return "Pretested Integration publisher to push to integration branch";
         }
 
         /**
