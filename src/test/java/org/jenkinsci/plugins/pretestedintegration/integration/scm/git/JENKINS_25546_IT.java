f5e0857fede30319b023398874eb393d46188f1a
Simon Grønborg <groenborg27@gmail.com>
2017-12-06 15:14:44 +0100

    fix #92 deleted multiscm remote tests
    	Tests are no longer valid. See comment on issue on GitHub
     Changes to be committed:
    	modified:   src/test/java/org/jenkinsci/plugins/pretestedintegration/integration/scm/git/JENKINS_24754_IT.java
    	modified:   src/test/java/org/jenkinsci/plugins/pretestedintegration/integration/scm/git/JENKINS_25546_IT.java

--- src/test/java/org/jenkinsci/plugins/pretestedintegration/integration/scm/git/JENKINS_25546_IT.java
+++ src/test/java/org/jenkinsci/plugins/pretestedintegration/integration/scm/git/JENKINS_25546_IT.java
@@ -27,69 +27,7 @@ public class JENKINS_25546_IT {
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
 
-    /**
-     * Branch specifier for all are the same.
-     *
-     * Pretested integration repo name
-     *  - blank/origin
-     *
-     * MultiSCM configuration
-     *  - GitSCM 
-     *      - 1 repo: named origin
-     *  - GitSCM
-     *      - 1 repo: named origin
-     *
-     * Workflow
-     *  1. Let the build(s) finish.
-     *  2. Once finished commit a new branch on the first repository
-     *
-     * Expected results
-     *  We expect the build to fail. Because the remote (origin) cannot be determined.
-     *
-     * @throws Exception
-     */
-    @Test
-    public void ambiguityInMultiSCMRemote() throws Exception {
-        Repository repo1 = TestUtilsFactory.createValidRepository("scenario1-1");
-        Repository repo2 = TestUtilsFactory.createValidRepository("scenario1-2");
 
-        List<GitSCMExtension> gitSCMExtensions = new ArrayList<>();
-        gitSCMExtensions.add(new PruneStaleBranch());
-        gitSCMExtensions.add(new CleanCheckout());
-
-        SCM gitSCM1 = new GitSCM(Collections.singletonList(new UserRemoteConfig("file://" + repo1.getDirectory().getAbsolutePath(), "origin", null, null)),
-                Collections.singletonList(new BranchSpec("*/ready/**")),
-                false, Collections.<SubmoduleConfig>emptyList(),
-                null, null, gitSCMExtensions);
-
-        SCM gitSCM2 = new GitSCM(Collections.singletonList(new UserRemoteConfig("file://" + repo2.getDirectory().getAbsolutePath(), "origin", null, null)),
-                Collections.singletonList(new BranchSpec("*/ready/**")),
-                false, Collections.<SubmoduleConfig>emptyList(),
-                null, null, gitSCMExtensions);
-
-        FreeStyleProject project = TestUtilsFactory.configurePretestedIntegrationPluginWithMultiSCM(jenkinsRule, TestUtilsFactory.STRATEGY_TYPE.SQUASH, Arrays.asList(gitSCM1, gitSCM2), "origin");
-        TestUtilsFactory.triggerProject(project);
-
-        jenkinsRule.waitUntilNoActivityUpTo(60000);
-
-        new UniqueBranchGenerator(repo1, "One", "Two", "Three").usingBranch("ready/scenario1").build();
-
-        TestUtilsFactory.triggerProject(project);
-
-        jenkinsRule.waitUntilNoActivityUpTo(60000);
-
-        TestUtilsFactory.destroyRepo(repo2);
-
-        AbstractBuild<?, ?> lastBuild = project.getLastBuild();
-
-        String consoleOut = jenkinsRule.createWebClient().getPage(lastBuild, "console").asText();
-
-        try (BuildResultValidator validator = new BuildResultValidator(lastBuild, repo1, consoleOut)) {
-            TestUtilsFactory.destroyRepo(repo2);
-            validator.hasResult(Result.FAILURE).buildLogContains(UnsupportedConfigurationException.AMBIGUITY_IN_REMOTE_NAMES).validate();
-        }
-
-    }
 
     /**
      * Branch specifier for all are the same.
@@ -159,67 +97,7 @@ public class JENKINS_25546_IT {
 
     }
 
-    /**
-     * Branch specifier for all are the same . Pretested integration repository
-     * name - stable
-     *
-     * MultiSCM
-     *  - GitSCM
-     *      - stable
-     *      - stable-two (default)
-     *  - GitSCM
-     *      - stable 
-     *
-     * Workflow
-     *  1. Create a branch named ready/scenario3
-     *  2. Push branch
-     *
-     * Results
-     *  - We expect to fail because the integration repository is not unique, and 
-     *    is present in both GitSCM's
 
-     * @throws Exception
-     */
-    @Test
-    public void ambiguityMultiSCMIntegrationTarget() throws Exception {
-        Repository repo1 = TestUtilsFactory.createValidRepository("scenario3-1");
-        Repository repo2 = TestUtilsFactory.createValidRepository("scenario3-2");
-        Repository repo3 = TestUtilsFactory.createValidRepository("scenario3-3");
-
-        List<GitSCMExtension> gitSCMExtensions = new ArrayList<>();
-        gitSCMExtensions.add(new PruneStaleBranch());
-        gitSCMExtensions.add(new CleanCheckout());
-
-        SCM gitSCM1 = new GitSCM(Arrays.asList(new UserRemoteConfig("file://" + repo1.getDirectory().getAbsolutePath(), "stable", null, null), new UserRemoteConfig("file://" + repo3.getDirectory().getAbsolutePath(), "stable-two", null, null)),
-                Collections.singletonList(new BranchSpec("*/ready/**")),
-                false, Collections.<SubmoduleConfig>emptyList(),
-                null, null, gitSCMExtensions);
-
-        SCM gitSCM2 = new GitSCM(Collections.singletonList(new UserRemoteConfig("file://" + repo2.getDirectory().getAbsolutePath(), "stable", null, null)),
-                Collections.singletonList(new BranchSpec("*/ready/**")),
-                false, Collections.<SubmoduleConfig>emptyList(),
-                null, null, gitSCMExtensions);
-
-        FreeStyleProject project = TestUtilsFactory.configurePretestedIntegrationPluginWithMultiSCM(jenkinsRule, TestUtilsFactory.STRATEGY_TYPE.SQUASH, Arrays.asList(gitSCM1, gitSCM2), "stable");
-        TestUtilsFactory.triggerProject(project);
-
-        jenkinsRule.waitUntilNoActivityUpTo(45000);
-
-        new UniqueBranchGenerator(repo1, "Message1", "Message2").usingBranch("ready/scenario3").build();
-
-        TestUtilsFactory.triggerProject(project);
-
-        jenkinsRule.waitUntilNoActivityUpTo(45000);
-
-        String console = jenkinsRule.createWebClient().getPage(project.getLastBuild(), "console").asText();
-
-        try (BuildResultValidator validator = new BuildResultValidator(project.getLastBuild(), repo1, console)) {
-            TestUtilsFactory.destroyRepo(repo2, repo3);
-            validator.buildLogContains(UnsupportedConfigurationException.AMBIGUITY_IN_REMOTE_NAMES)
-                    .hasResult(Result.FAILURE).validate();
-        }
-
-    }
 
     /**
      * Branch specifier for all are the same.
